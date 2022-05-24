4371412b577fed635cbe1c165a580d6374b39e53
Acelya Uslu <ace.uslu@gmail.com>
2022-05-24 18:04:52 +0200

    sign-up with new inputs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  include Pundit::Authorization
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit::Authorization
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   private
+
+  def configure_permitted_parameters
+    attributes = [:first_name, :last_name, :date_of_birth]
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+  end
 
   def skip_pundit?
     devise_controller?
