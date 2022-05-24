98f289d99983aa204c85e02407794ec1e447f028
edouard doin <edouarddoin@gmail.com>
2022-05-24 18:04:58 +0200

    profile utilisateur

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: 'products#index'
-
+  get '/profile', to: 'profiles#profile'
   resources :products
 end
