70892141b3e0133a0127d2c6747f93985b5545ca
adams laurent <adams.khalfi@gmail.com>
2022-05-24 14:55:30 +0200

    acces show sans login

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
 
   def index
     @products = policy_scope(Product)
