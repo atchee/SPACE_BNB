6e52cf77988975d3255dbc1afb80e9eb931aadce
Acelya Uslu <ace.uslu@gmail.com>
2022-05-26 12:57:55 +0200

    wip

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="alert"
 export default class extends Controller {
+  static targets = ['notice']
+
   connect() {
     console.log('hello')
     console.log(this.element)
     this.element.addEventListener("click", function() {
-      alert("Please login order to book this place");
-  });
+      this.noticeTarget.innerText = 'Please login order to book this place'
+    });
   }
 }
