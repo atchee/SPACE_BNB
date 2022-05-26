45a7e25426c02816f9417ea19d1dbda1172a98b1
adams laurent <adams.khalfi@gmail.com>
2022-05-26 15:05:44 +0200

    button fixed

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,8 +2,6 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 import { application } from "./application"
-import NavbarController from "./navbar_controller.js"
-application.register("navbar", NavbarController)
 
 import AlertController from "./alert_controller.js"
 application.register("alert", AlertController)
