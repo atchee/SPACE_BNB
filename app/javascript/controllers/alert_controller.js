5e86e1ea10fc586ab5a04963653bb1a2846e180a
Acelya Uslu <ace.uslu@gmail.com>
2022-05-26 13:10:43 +0200

    alert plus propre

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -2,13 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="alert"
 export default class extends Controller {
-  static targets = ['notice']
+  static targets = ['notice', 'button']
 
-  connect() {
-    console.log('hello')
-    console.log(this.element)
-    this.element.addEventListener("click", function() {
-      this.noticeTarget.innerText = 'Please login order to book this place'
-    });
+  notice(evt) {
+    evt.preventDefault();
+    this.noticeTarget.innerText = 'Please login in order to book this place'
   }
 }
